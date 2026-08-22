b3274e6a7342d5705112f5afeca632805b0b6739
jayis1 <jaisbygebjerg@gmail.com>
2026-08-22 00:59:35 +0000

    chore: remove Pipe Halo build artifacts

--- pipe-halo/firmware/build/CMakeFiles/pipe_halo_demo.dir/src/sensors.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/pipe_halo_demo.dir/src/sensors.c.o: \
- /root/SoC-Device-Inventions/pipe-halo/firmware/src/sensors.c \
- /usr/include/stdc-predef.h \
- /root/SoC-Device-Inventions/pipe-halo/firmware/include/sensors.h \
- /root/SoC-Device-Inventions/pipe-halo/firmware/include/pipe_halo.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stddef.h
